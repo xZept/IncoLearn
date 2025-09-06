5b1d015d683d32bc00129f3b4f72f0148ca071b8
xZept <allenjames.laxamana@gmail.com>
2025-09-06 12:36:17 +0800

    Format parameter to string and make it a tuple

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ async def fetch_id(column_name, table_name, row_name, row_value):
             retrieved_value = cur.fetchone()
             cur.close()
             if retrieved_value:
-                print("Retrieved value:", retrieved_value[0])
+                print("Retrieved value:", str(retrieved_value[0]),)
                 return retrieved_value[0]
             else:
                 print("No value found!")
